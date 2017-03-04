4b46e8fce3e2cb478e28dcf314b2ba44e6b30e7a
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2017-03-04 10:25:10 -0600

    adding service calls for contact form

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -1,5 +1,5 @@
 angular.module('professionalWebsite')
-    .controller('ContactController', ['$rootScope','$scope', '$window', '$state', function($rootScope, $scope, $window, $state) {
+    .controller('ContactController', ['$rootScope','$scope', '$window', '$state', '$http', function($rootScope, $scope, $window, $state, $http) {
         
     $scope.user = {
         to: "deonawilliams26@gmail.com",
@@ -9,24 +9,30 @@ angular.module('professionalWebsite')
         message: ""
     } 
     
-    $scope.sendEmail = function() {
-        console.log($scope.user);
-        $scope.user = null;
-        alert("Thank you for your message");
-    }        
-
-        
+    $scope.sendEmail = function(user) {
+        $http({
+            method: 'POST',
+            url: '/webformmailer.php',
+            data: {
+                to: user.to,
+                name: user.name,
+                email: user.email,
+                subject: user.subject,
+                comments: user.message
+            }
+        }).then(function successCallback(data) {
+            console.log($scope.user);
+            $scope.user = null;
+            alert("Thank you for your message");
+            // when the response is available
+        }, function errorCallback(data) {
+             console.log("Form not submitted");
+             console.log($scope.user);
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+       
+    }   
     console.log("Contact controller works!");
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
     }]);
