fb8a6a73fadafccad21bc57c8d2776bf442f861b
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2017-03-15 20:54:32 -0500

    the email works now

--- contact.php
+++ contact.php
@@ -2,23 +2,25 @@
     $formData = file_get_contents( 'php://input' );
     $data = json_decode($formData);
     if ($data) {
-        $name = $data->name;
-        $email = $data->email;
-        $sendermessage = $data->subject;
+        $sendername = $data->name;
+        $sendersubject = $data->subject;
+        $senderemail = $data->email;
         $message = $data->message;
 
-        if ( $name != '') {
+        if ( $sendername != '') {
             $mailTo = 'contact@deonawilliams.com';
             $subject = 'New contact form submission';
-            $body  = 'From: ' . $name . "\n";
-            $body  = 'Subject: ' . $sendermessage . "\n";
-            $body .= 'Email: ' . $email . "\n";
+            $body  = 'From: ' . $sendername . "\n";
+            $body  = 'Subject: ' . $sendersubject . "\n";
+            $body .= 'Email: ' . $senderemail . "\n";
             $body .= "Message:\n" . $message . "\n\n";
 
-            $success = mail( $mailTo, $subject, $body );
+            $sent = mail( $mailTo, $subject, $body );
 
-            if ( $success ) {
-                $response[ 'success' ] = true;
+            if ($sent) {
+                print("Success");
+            }else {
+                print("Failed");
             }
         }
     }
