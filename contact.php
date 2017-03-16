98bf80a4f3b9b67457189e0ea19523c94cc969f8
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2017-03-15 21:18:47 -0500

    all fields are now showing up in email

--- contact.php
+++ contact.php
@@ -1,27 +1,22 @@
 <?php
-    $formData = file_get_contents( 'php://input' );
-    $data = json_decode($formData);
+    $contactFormData = file_get_contents( 'php://input' );
+    $data = json_decode($contactFormData);
     if ($data) {
-        $sendername = $data->name;
+        $sendername = $data->contactname;
         $sendersubject = $data->subject;
         $senderemail = $data->email;
         $message = $data->message;
-
+        
         if ( $sendername != '') {
             $mailTo = 'contact@deonawilliams.com';
             $subject = 'New contact form submission';
-            $body  = 'From: ' . $sendername . "\n";
-            $body  = 'Subject: ' . $sendersubject . "\n";
+            $body  .= 'From: ' . $sendername . "\n";
+            $body  .= 'Subject: ' . $sendersubject . "\n";
             $body .= 'Email: ' . $senderemail . "\n";
             $body .= "Message:\n" . $message . "\n\n";
 
             $sent = mail( $mailTo, $subject, $body );
 
-            if ($sent) {
-                print("Success");
-            }else {
-                print("Failed");
-            }
         }
     }
 ?>
