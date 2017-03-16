b1ad7b0906d357716be527e18682bc345ded75da
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2017-03-15 20:30:35 -0500

    halfway working form

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -1,21 +1,20 @@
 angular.module('professionalWebsite')
     .controller('ContactController', ['$rootScope','$scope', '$window', '$state', '$http', function($rootScope, $scope, $window, $state, $http) {
-        
-    $scope.user = {};
     
-    $scope.sendEmail = function(user) {
+    $scope.sendEmail = function(input) {
         $http({
             method: 'POST',
             url: 'contact.php',
-            data: $scope.user
+            data: input,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
         }).then(function successCallback(data) {
-            console.log($scope.user);
+            console.log(input);
             console.log(data);
             $scope.user = null;
             // when the response is available
         }, function errorCallback(data) {
              console.log("Form not submitted");
-             console.log($scope.user);
+             console.log(input);
              console.log(data);                    
             // called asynchronously if an error occurs
             // or server returns response with an error status.
--- contact.php
+++ contact.php
@@ -1,36 +1,25 @@
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
-
 <?php
-   //$to = "Deona Williams <contact@deonawilliams.com>"; 
-   //$from = "contact@deonawilliams.com";         
-   $name = $_POST["name"];
-   $subject = $_POST["subject"];       
-   $message = $_POST["message"];
-   $email = $_POST["email"];
-   $headers = "From: contact@deonawilliams.com" . "\r\n" .
-    "X-Mailer: PHP/" . phpversion();
+    $formData = file_get_contents( 'php://input' );
+    $data = json_decode($formData);
+    if ($data) {
+        $name = $data->name;
+        $email = $data->email;
+        $sendermessage = $data->subject;
+        $message = $data->message;
 
-    $message ="";
-    $message .="Name : ";
-    $message .=$name;
-    //$.body .="\n";
-    $message .="Subject : ";
-    $message .=$subject;
-    //$.body .="\n";
-    $message .="Message : ";
-    $message .=$message;
-    //$.body .="\n";
-    $message .="Email : ";
-    $message .=$email;
-    //$.body .="\n";
+        if ( $name != '') {
+            $mailTo = 'contact@deonawilliams.com';
+            $subject = 'New contact form submission';
+            $body  = 'From: ' . $name . "\n";
+            $body  = 'Subject: ' . $sendermessage . "\n";
+            $body .= 'Email: ' . $email . "\n";
+            $body .= "Message:\n" . $message . "\n\n";
 
-$go = mail('contact@deonawilliams.com', $subject, $message, $headers);
+            $success = mail( $mailTo, $subject, $body );
 
-if($go) {
-    print("Success");
-} else {
-    print("Failed to send");
-}
+            if ( $success ) {
+                $response[ 'success' ] = true;
+            }
+        }
+    }
 ?>
