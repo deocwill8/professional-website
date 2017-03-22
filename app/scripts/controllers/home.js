c32f72f27674fd40ff1da76bede62ff7ee90f507
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2017-03-21 21:27:50 -0500

    restyling layout

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,8 +1,6 @@
 angular.module('professionalWebsite')
     .controller('HomeController', ['$rootScope','$scope', '$window', '$state', function($rootScope, $scope, $window, $state) {
      $scope.home = true;    
-      $scope.contactMe = function() {
-          $state.go('contact')
-      }
-        console.log("Home controller works!");
+    
+     console.log("Home controller works!");
     }]);
--- app/scripts/controllers/intro.js
+++ app/scripts/controllers/intro.js
@@ -1,9 +1,9 @@
 angular.module('professionalWebsite')
     .controller('IntroController', ['$rootScope','$scope', '$window', '$state', function($rootScope, $scope, $window, $state) {
      $scope.intro = true; 
-      $scope.home = function() {
-          $state.go('Deona');
-      };    
+       $scope.seeMore = function() {
+          $state.go('Deona')
+      }
       
      console.log("Intro controller works!");
     }]);
