05a9f188395880445ee3b64c20976413d3819b40
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2017-03-21 22:17:43 -0500

    more layout changes

--- app/scripts/controllers/intro.js
+++ app/scripts/controllers/intro.js
@@ -2,8 +2,11 @@ angular.module('professionalWebsite')
     .controller('IntroController', ['$rootScope','$scope', '$window', '$state', function($rootScope, $scope, $window, $state) {
      $scope.intro = true; 
        $scope.seeMore = function() {
-          $state.go('Deona')
+          $state.go('Deona');
       }
+       $scope.contact = function() {
+          $state.go('contact');
+      }   
       
      console.log("Intro controller works!");
     }]);
