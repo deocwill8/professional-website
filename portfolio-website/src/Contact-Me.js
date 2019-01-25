89a05080bb114c2b5ac29b9bbc72e96f0a7cc292
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2019-01-25 07:50:55 -0600

    added markup for projects page and contact page

--- portfolio-website/src/Contact-Me.js
+++ portfolio-website/src/Contact-Me.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
+import './Contact-Me.css'
 
 class ContactMe extends Component {
   render() {
     return (
       <div>
-        <p>Contact Me</p>
+        <Route path="/contact-me" render={() => (
+          <div className="centered">
+            <div className="contact-me-wrapper">
+
+              <div className="container1">
+                <div className="container1-heading-container">
+                  <h2 className="contact-me-header">Lets Talk About</h2>
+                  <h4 className="contact-me-about-text">Job Opprotunites</h4>
+                </div>
+                <div className="container1-contact-info-container">
+                  <p>Email me at: something@gmail.com</p> 
+                  <p>Follow me on Github</p> 
+                  <p>Follow me on LinkedIn</p>  
+                </div>
+              </div>
+
+              <div className="container2">
+                <div className="contact-form-group">
+                  <form action="" method="post" id="contact-form">
+                      <div>
+                        <label for="first_name">First Name</label>
+                        <input type="text" id="first_name" placeholder="first name" name="user_first_name" />
+                      </div>
+                      <div>
+                          <label for="last_name">Last Name</label>
+                          <input type="text" id="last_name" placeholder="last name" name="user_last_name" />
+                        </div>
+                      <div>
+                        <label for="mail">Email</label>
+                        <input type="email" id="mail" placeholder="email" name="user_mail" />
+                      </div>
+                      <div>
+                        <label for="msg">Message</label>
+                        <textarea id="msg" placeholder="please type your message here..." name="user_message"></textarea>
+                      </div>
+                      <button type="submit" name="submit" id="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+
+          </div>
+
+        </div>
+      )} />
+       
       </div>
     );
   }
--- portfolio-website/src/Projects.js
+++ portfolio-website/src/Projects.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
+import profilePicture from './deona1.jpg'
+import './Projects.css'
 
 class Projects extends Component {
   render() {
     return (
       <div>
-        <p>Projects</p>
+
+        <Route path="/projects" render={() => (
+          <div className="centered">
+            <div className="cards">
+              <article className="card">
+                  <div className="img-container">
+                      <img src={profilePicture} alt="deona" />
+                  </div>
+                  <h2 className="title">Restaurant Reviews</h2>
+                  <div className="card-info-container">
+                      <p className="card-content">
+                      is simply dummy text of the printing and typesetting industry. 
+                      Lorem Ipsum has been the industry's standard dummy text ever since 
+                      the 1500s, when an unknown printer took a galley of type and  
+                      is simply dummy text of the printing and typesetting industry. 
+                      Lorem Ipsum has been the industry's standard dummy text ever since 
+                      the 1500s, when an unknown printer took a galley of type and  
+                      </p>
+                  </div>
+              </article>
+            </div>
+          </div>
+        )} />
+        
       </div>
     );
   }
