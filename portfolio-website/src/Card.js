1b03a3f6a8b75c584bed4de7568b3d3976405f27
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2019-02-20 17:42:23 -0600

    now reading image uri from json

--- portfolio-website/src/Card.js
+++ portfolio-website/src/Card.js
@@ -7,8 +7,7 @@ class Card extends Component {
     return (
       <article className="card">
             <div className="img-container">
-                {/* <img src={this.props.cardData.img} alt={this.props.cardData.alt} /> */}
-                <img src={this.props.cardData.img} alt={this.props.cardData.alt}></img> 
+                <img src={this.props.cardData.img} alt={this.props.cardData.alt} />
             </div>
             <h2 className="title">{this.props.cardData.name}</h2>
             <div className="card-info-container">
