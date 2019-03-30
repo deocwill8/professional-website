ab5ef589eaa347d54c8c9629197d36c13434e66f
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2019-03-30 14:12:38 -0500

    projects page now scrolls and aligning cards properly

--- portfolio-website/src/projects-by-me/Projects.js
+++ portfolio-website/src/projects-by-me/Projects.js
@@ -9,17 +9,17 @@ import './Projects.css'
 class Projects extends Component {
   render() {
     return (
-      <div id="projects">
+      <div id="projects" className="centered">
         <Navbar />
         <SideBar pageWrapId={"page-wrap"} outerContainerId={"projects"} />
         <Route path="/projects" render={() => (
-          <div id="page-wrap" className="centered">
+          <div id="page-wrap" className="center-cards"> 
             <div className="cards">
               {this.props.projects.map((project) => (
                   <Card cardData={project} key={project.id}></Card>
               ))}
             </div>
-          </div>
+          </div>  
         )} />
       </div>
     );
