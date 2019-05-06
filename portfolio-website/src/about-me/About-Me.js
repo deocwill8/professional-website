722d915a489f3c5984541bcb73973d76ce437f01
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2019-05-06 07:58:22 -0500

    skeloton for mobile about me

--- portfolio-website/src/about-me/About-Me.js
+++ portfolio-website/src/about-me/About-Me.js
@@ -14,21 +14,24 @@ class AboutMe extends Component {
         <Route path="/about-me" render={() => (
             <div id="page-wrap">
               <div className="about-me-wrapper">
+                {/* <div className="nav-container">
+                  <h2>Deona Williams</h2>
+                </div> */}
                 <div className="about-me-container">
                   <div className="container-one">
                     <img className="about-me-picture" src={profilePicture} alt="deona" />
                   </div>
                   <div className="container-two">
                     <p className="about-me-text">
-                    First paragraph. is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since 
-                    the 1500s, when an unknown printer took a galley of type and 
-                    scrambled it to make a type specimen book. It has survived not 
-                    only five centuries, but also the leap into electronic typesetting, 
-                    remaining essentially unchanged. It was popularised in the 1960s 
-                    with the release of Letraset sheets containing Lorem Ipsum passages, 
-                    and more recently with desktop publishing software like Aldus 
-                    PageMaker including versions of Lorem Ipsum. End
+                    As a passionate web developer I find magic in every line of code I write. 
+                    I love seeing things come to fruition in the browser and on web applications. 
+                    I have been focusing mainly on front-end languages 
+                    and technologies.I am originally from East Chicago, Indiana and studied Informatics 
+                    at Indiana University in Bloomington, Indiana. Taking Human Computer Interaction (HCI) courses there is what 
+                    initiated my aspiration to create technologies and products to help people.
+                    Ive currently worked with HTML/CSS, Bootstrap, JavaScript, AngularJS, PHP, and web 
+                    build/automation tools. You may view my source code on GitHub by clicking on 
+                    the link below. 
                     </p>  
                   </div>
                   <div className="container-three">
