f33f0d8b2a793b3cd6367fa535aa191dd2fe2f91
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2019-10-07 22:50:02 -0500

    install react icons, add icons to contact page

--- portfolio-website/src/contact-me/Contact-Me.js
+++ portfolio-website/src/contact-me/Contact-Me.js
@@ -3,8 +3,17 @@ import { Route } from 'react-router-dom'
 import Navbar from '../components/navbar/navbar'
 import SideBar from '../components/sidebar/sidebar'
 import axios from 'axios'
+import { IconContext } from "react-icons";
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithubSquare } from 'react-icons/fa'
+import { FaEnvelopeOpenText }  from 'react-icons/fa'
 import './Contact-Me.css'
 
+{/* <IconContext.Provider value={{ color: "blue" }}>
+<div>
+  <FaEnvelopeOpenText />
+</div>
+</IconContext.Provider> */}
 
 class ContactMe extends Component {
 
@@ -19,6 +28,8 @@ class ContactMe extends Component {
       wordIdx: 0
     }
   }
+
+
 
   handleForm = e => {
     axios.post ("https://formcarry.com/s/fAi3QyjgPQb",
@@ -42,6 +53,7 @@ class ContactMe extends Component {
   render() {
 
     return (
+
       <div id="contactMe">
       <Navbar />
       <SideBar pageWrapId={"page-wrap"} outerContainerId={"contactMe"} />
@@ -50,20 +62,23 @@ class ContactMe extends Component {
             <div className="contact-me-wrapper">
               <div className="contact-page-title">
                 <h1>About</h1>
-                <hr className="divider"></hr>
               </div>
               <div className="contact-content-container">
             {/* container 1 */}
 
               <div className="container1">
                 <div className="container1-heading-container">
-                  <h2 className="contact-me-header">Lets Talk About</h2>
-                  <h4 className="contact-me-about-text">Web opprotunities</h4>
+                  <h2>Let's keep in touch!</h2>
+                  <h4>I will be sure to reply back to you!</h4>
                 </div>
                 <div className="container1-contact-info-container">
-                  <p>Email me at: something@gmail.com</p> 
-                  <p>Follow me on Github</p> 
-                  <p>Follow me on LinkedIn</p>  
+                  <IconContext.Provider value={{ size: '20px' }}>
+                    <div>
+                      <p> <FaEnvelopeOpenText /> something@gmail.com</p> 
+                      <p> Follow @  <FaGithubSquare /> </p> 
+                      <p>Follow @ <FaLinkedin /></p>  
+                    </div>
+                  </IconContext.Provider>
                 </div>
               </div>
 
@@ -73,19 +88,19 @@ class ContactMe extends Component {
                   <form onSubmit={this.handleForm} id="contact-form">
                       <div>
                         <label htmlFor="first_name">First Name</label>
-                        <input type="text" id="first_name" placeholder="first name" name="sender_first_name" onChange={this.handleFields} />
+                        <input type="text" id="first_name" name="sender_first_name" onChange={this.handleFields} />
                       </div>
                       <div>
                           <label htmlFor="last_name">Last Name</label>
-                          <input type="text" id="last_name" placeholder="last name" name="sender_last_name" onChange={this.handleFields} />
+                          <input type="text" id="last_name" name="sender_last_name" onChange={this.handleFields} />
                         </div>
                       <div>
                         <label htmlFor="mail">Email</label>
-                        <input type="email" id="mail" placeholder="email" name="sender_mail" onChange={this.handleFields} />
+                        <input type="email" id="mail" name="sender_mail" onChange={this.handleFields} />
                       </div>
                       <div>
                         <label htmlFor="msg">Message</label>
-                        <textarea id="msg" placeholder="please type your message here..." name="sender_message" onChange={this.handleFields}></textarea>
+                        <textarea id="msg" name="sender_message" onChange={this.handleFields}></textarea>
                       </div>
                       <button type="submit" name="submit" id="submit">Submit</button>
                     </form>
